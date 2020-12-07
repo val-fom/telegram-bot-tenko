4034419bb21470d11b626e9902ff49b499f4f15c
Valentyn Fomenko <fomenko.vn@gmail.com>
2020-12-07 14:46:46 +0200

    make the bot private

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@ const rp = require('request-promise');
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
 const TENKO_API_URI = process.env.TENKO_API_URI
 const TENKO_API_TOKEN = process.env.TENKO_API_TOKEN
+const USER_NAME = process.env.USER_NAME
 
 async function getTotalState() {
   const options = {
@@ -34,13 +35,17 @@ module.exports.tenkobot = async event => {
   const body = JSON.parse(event.body);
   const {chat, text} = body.message || body.edited_message;
 
+  if (USER_NAME && chat.username !== USER_NAME) {
+    await sendToUser(chat.id, 'This is a private bot');
+    console.warn(`User @${chat.username} tried to chat with me`);
+    return { statusCode: 200 };
+  }
+
   if (text === '/total_state') {
     let message = '';
     try {
       const result = await getTotalState();
-      console.log('result: ', result);
       message = JSON.stringify(result, null, 2);
-      console.log('message: ', message);
     } catch (error) {
       message = `Input: ${text}, \nError: ${error.message}`;
     }
